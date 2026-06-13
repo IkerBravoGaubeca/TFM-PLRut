524d50ddd181c639330002f8108a624149607d64
Iker Bravo Gaubeca <125264092+IkerBravoGaubeca@users.noreply.github.com>
2026-06-13 19:10:47 +0200

    Add files via upload

---
+++ PLRutv3/CapacityServer.h
@@ -0,0 +1,132 @@
+#ifndef _CAPACITYSERVER_H_
+#define _CAPACITYSERVER_H_
+
+#include <list>
+#include <queue>
+#include <map>
+#include <set>
+#include <vector>
+#include <string>
+
+#include <omnetpp.h>
+
+#include <inet/networklayer/common/L3AddressResolver.h>
+#include <inet/networklayer/common/L3AddressTag_m.h>
+#include <inet/transportlayer/contract/udp/UdpSocket.h>
+
+#include "apps/PLRut/CapacityPackets_m.h"
+
+#include "veins/modules/mobility/traci/TraCIMobility.h"
+#include "veins/modules/mobility/traci/TraCICommandInterface.h"
+#include "veins_inet/VeinsInetMobility.h"
+
+using veins::VeinsInetMobility;
+using namespace std;
+using namespace inet;
+using namespace veins;
+
+struct PendingReply {
+    std::string carId;
+    inet::L3Address destAddr;
+    std::string newRouteEdges;
+    bool hasNewRoute;
+    bool wait;
+};
+
+struct WaitingCar {
+    std::string     carId;
+    inet::L3Address destAddr;
+    std::string     fromEdge;
+    std::string     toEdge;
+    std::vector<std::string> originalRoute;
+};
+
+class CapacityServer : public omnetpp::cSimpleModule
+{
+    inet::UdpSocket socket;
+
+    int iDtalk_;
+    int size_;
+    int localPort_;
+    int destPort_;
+
+    unsigned int totalSentBytes_;
+    omnetpp::simsignal_t capacityGeneratedThroughtput_;
+
+    omnetpp::cMessage *selfSender_;
+
+    std::queue<PendingReply> replyQueue_;
+
+    std::string mapName;
+    std::string duarouterPath_;
+    std::string tmpDir_;
+    int defaultCapacity_;
+    bool enableMaxDistance_;
+    int maxDistance_;
+
+    ~CapacityServer();
+
+    typedef std::list<CapacityPacket*> PacketsList;
+    PacketsList mPacketsList_;
+    PacketsList mPlayoutQueue_;
+    unsigned int mCurrentTalkspurt_;
+
+    bool mInit_;
+    unsigned int totalRcvdBytes_;
+
+    void enqueueReply(const PendingReply& reply);
+    void sendNextReply();
+
+    std::set<std::string> getBlockedEdges();
+    void loadCapacityJson(const std::string& path);
+    std::vector<std::string> parseRoute(const std::string& routeStr);
+    std::string joinRoute(const std::vector<std::string>& route);
+
+    void reserveRoute(const std::string& carId, const std::vector<std::string>& route);
+    void releaseRoute(const std::string& carId);
+
+    std::vector<std::string> computeWindow(const std::vector<std::string>& fullRoute,
+                                           const std::string& currentEdge);
+    void updateWindow(const std::string& carId, const std::string& currentEdge);
+    void reserveWindow(const std::string& carId, const std::vector<std::string>& window);
+    bool windowBlockedForCar(const std::string& carId, const std::vector<std::string>& window);
+
+    bool routeBlockedForCar(const std::string& carId, const std::vector<std::string>& route);
+    int getCapacityForEdge(const std::string& edge);
+
+    std::vector<std::string> computeAlternativeRoute(
+        const std::string& fromEdge,
+        const std::string& toEdge,
+        const std::set<std::string>& blockedEdges,
+        const std::string& carId);
+
+    void writeTripsFile(const std::string& tripsFile,
+                        const std::string& fromEdge,
+                        const std::string& toEdge);
+
+    void markEdgesAsBlocked(const std::string& filename,
+                            const std::set<std::string>& blockedEdges);
+
+    void retryWaitingCars();
+
+    cModule* findCarModule(const std::string& carId);
+
+public:
+    CapacityServer();
+
+protected:
+    std::map<std::string, int>                       edgeCapacity_;
+    std::map<std::string, int>                       edgeOccupation_;
+    std::map<std::string, std::vector<std::string>>  reservedRoutesByCar_;
+    std::map<std::string, std::vector<std::string>>  fullRouteByCar_;
+    std::map<std::string, std::string>               originalDestinationByCar_;
+    std::list<WaitingCar>                            waitingCars_;
+
+    std::map<std::string, int>                       omnetIndexByCar_;
+
+    virtual int numInitStages() const override { return inet::NUM_INIT_STAGES; }
+    void initialize(int stage) override;
+    void handleMessage(omnetpp::cMessage *msg) override;
+};
+
+#endif
---
+++ PLRutv3/CapacityVehicle.h
@@ -0,0 +1,89 @@
+#ifndef _CAPACITYVEHICLE_H_
+#define _CAPACITYVEHICLE_H_
+
+#include <list>
+#include <string>
+
+#include <omnetpp.h>
+
+#include <inet/networklayer/common/L3AddressResolver.h>
+#include <inet/transportlayer/contract/udp/UdpSocket.h>
+#include <inet/common/packet/Packet.h>
+#include <inet/common/Ptr.h>
+#include <inet/common/TagBase.h>
+#include "apps/PLRut/CapacityPackets_m.h"
+
+#include "veins/modules/mobility/traci/TraCIMobility.h"
+#include "veins/modules/mobility/traci/TraCICommandInterface.h"
+#include "veins_inet/VeinsInetMobility.h"
+
+using veins::VeinsInetMobility;
+using namespace std;
+using namespace inet;
+using namespace veins;
+
+class CapacityVehicle : public omnetpp::cSimpleModule,
+                        public inet::UdpSocket::ICallback
+{
+    inet::UdpSocket socket;
+
+    omnetpp::simtime_t durTalk_;
+    omnetpp::cMessage* selfSource_;
+    omnetpp::cMessage* selfSender_;
+    omnetpp::cMessage* initTraffic_;
+
+    int iDtalk_;
+    int size_;
+    std::string navMessage_;
+
+    unsigned int totalSentBytes_;
+    omnetpp::simsignal_t capacityGeneratedThroughtput_;
+
+    omnetpp::simtime_t timestamp_;
+    omnetpp::simtime_t startTime_;
+    int localPort_;
+    int destPort_;
+    inet::L3Address destAddress_;
+
+    bool routeSent_;
+    bool arrivedSent_;
+    bool isStopped_;
+
+    ~CapacityVehicle();
+
+    typedef std::list<CapacityPacket*> PacketsList;
+    PacketsList mPacketsList_;
+    PacketsList mPlayoutQueue_;
+    unsigned int mCurrentTalkspurt_;
+
+    bool mInit_;
+    unsigned int totalRcvdBytes_;
+
+    void initTraffic();
+    void selectPeriodTime();
+    void sendCapacityPacket();
+    void processReply(inet::Packet* pkt);
+
+public:
+    CapacityVehicle();
+
+    virtual void socketDataArrived(inet::UdpSocket* socket,
+                                   inet::Packet* packet) override;
+    virtual void socketErrorArrived(inet::UdpSocket* socket,
+                                    inet::Indication* indication) override {}
+    virtual void socketClosed(inet::UdpSocket* socket) override {}
+
+protected:
+    VeinsInetMobility* mobility;
+    TraCICommandInterface* traci;
+    TraCICommandInterface::Vehicle* traciVehicle;
+    std::string carId;
+    int omnetIndex_;
+
+    virtual int numInitStages() const override { return inet::NUM_INIT_STAGES; }
+    void initialize(int stage) override;
+    void handleMessage(omnetpp::cMessage *msg) override;
+    void finish() override;
+};
+
+#endif
